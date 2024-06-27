b502b518ed509416359e381c939ee6a5b744e215
Arturo Muller <amullersimons@gmail.com>
2024-06-27 15:07:03 -0500

    add: Header and css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from './Header.svelte'
+	import '../styles/global.css'
+</script>
+
+<Header />
+<main>
+	<slot></slot>
+</main>
+
+<style>
+    main {
+        max-width: 960px;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,31 @@
+<header>
+    <div class="title">
+        <div class="svg-container">
+            <img class="svg-image" src="/star-wars.svg" alt="site logo">
+        </div>
+        <h1>Technical Assessment</h1>
+    </div>
+</header>
+<style>
+    header {
+        background: #B0B0B0;
+        display: flex;
+        justify-content: center;
+    }
+    .title {
+        display: flex;
+        align-items: center;
+    }
+
+    .svg-container {
+        width: 100px;
+        height: 100px;
+        overflow: hidden;
+    }
+
+    .svg-image {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
