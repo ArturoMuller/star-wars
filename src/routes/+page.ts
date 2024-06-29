fe70998287e87976c9cc7d3cc9d60288e60d9546
Arturo Muller <amullersimons@gmail.com>
2024-06-29 13:37:35 -0500

    fix: build errors due to indexedDB init 2

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,7 @@ import { dbOps } from '$lib/db/store-utils';
 export async function load({ fetch, url, browser }) {
   const nextPage = url.searchParams.get('page') ?? SWAPI_PLANETS;
   if (browser) {
-    const cachedResults = await loadPlanets(nextPage);
+    const cachedResults = await dbOps.loadPlanets(nextPage);
     if (cachedResults) {
       const { results, next, previous } = cachedResults;
       return {
@@ -17,8 +17,6 @@ export async function load({ fetch, url, browser }) {
   const res = await fetch(nextPage);
   const { results, next, previous } = await res.json();
   if (browser) {
-    console.log('test store planet');
-
     dbOps.storePlanets(results, nextPage);
     dbOps.storePages({ page: nextPage, next, previous });
   }
